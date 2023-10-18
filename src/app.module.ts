7cedf6bbf96d4e8461930b47f1e92f69e8002e23
João Achão <96294746+felipeachao@users.noreply.github.com>
2023-10-18 19:51:17 -0400

    Initialize db/sequelize

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadModels: true,
+      synchronize: false,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
